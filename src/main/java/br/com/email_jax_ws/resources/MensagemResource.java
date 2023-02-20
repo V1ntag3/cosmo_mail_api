0a936ad430958690a544dbe02cf7e6c2e9985c7e
V1ntage <marcos.vinicius.r.alencar@gmail.com>
2023-02-20 18:39:58 -0300

    Ajuste nos métodos de Mensagens

--- src/main/java/br/com/email_jax_ws/resources/MensagemResource.java
+++ src/main/java/br/com/email_jax_ws/resources/MensagemResource.java
@@ -6,7 +6,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -28,15 +27,20 @@ public class MensagemResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response postEnviarMensagem(Mensagem msg) throws SQLException {
-        if (_repositorio.pegarUsuarioEmail(msg.getEmail_destinatario()) != null) {
+        Integer remetente_id = _repositorio.pegarUsuarioEmail(msg.getEmail_destinatario());
+        if (remetente_id != null) {
             return Response.status(Response.Status.NOT_FOUND).entity("Destinatário não encontrado").build();
         }
-        try {
-            _repositorio_men.mandarMensagem(msg);
-            return Response.status(Response.Status.OK).entity("Mensagem Enviada").build();
-        } catch (Exception ex) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+        else{
+            msg.setRemetente_id(remetente_id);
+            try {
+                _repositorio_men.mandarMensagem(msg);
+                return Response.status(Response.Status.OK).entity("Mensagem Enviada").build();
+            } catch (Exception ex) {
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+            }
         }
+       
     }
 
     @Authorize
@@ -62,34 +66,6 @@ public class MensagemResource {
         try {
             return Response.status(Response.Status.OK)
                     .entity("{\n \"mensagens\":" + _repositorio_men.mensagensRecebidas(user.getId()) + "}").build();
-        } catch (Exception ex) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
-        }
-    }
-
-    @Authorize
-    @PUT
-    @Path("update")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response postUpdate(Usuario usuario) {
-
-        try {
-            if (usuario.getNome() != "" && usuario.getNome() != null) {
-                _repositorio.updateUsuarioNome(usuario.getId(), usuario.getNome());
-
-            }
-
-            if (usuario.getTelefone() != "" && usuario.getTelefone() != null) {
-                _repositorio.updateUsuarioTelefone(usuario.getId(), usuario.getTelefone());
-            }
-
-            if (usuario.getSenha() != "" && usuario.getSenha() != null) {
-                _repositorio.updateUsuarioSenha(usuario.getId(), usuario.getSenha());
-            }
-
-            return Response.status(Response.Status.OK).entity("Usuário Atualizado").build();
-
         } catch (Exception ex) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
         }
